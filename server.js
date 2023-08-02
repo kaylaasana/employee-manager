b020771bbf90d5524e0f06acfda9ba8e0b81c3de
kayla <kayladatte@gmail.com>
2023-08-01 22:28:29 -0700

    initial file push

---
+++ server.js
@@ -0,0 +1,22 @@
+//import statements
+//create inquirer questions
+//switch statement
+
+
+const viewAllEmployees=()=>{
+    //query database
+    //display results
+    //return to main menu
+}
+    const addNewEmployee={
+}
+
+const addNewRole=()=>{
+    //add the name of the role
+    //ask the salary
+    //query departments
+        //ask department with list of choices
+        //write new data to db
+    //return confirmation message
+    //return to main menu
+}
