ab5287b10fdb46e59a12df01caf4e69f2539665c
kayla <kayladatte@gmail.com>
2023-08-02 20:03:46 -0700

    added comments to schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,20 @@
+-- create database
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
+-- access database
 USE employee_db;
 
+-- create table for departments
 DROP TABLE IF EXISTS departments;
 CREATE TABLE departments (
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(30) NOT NULL,
+    -- selecting the key of id to be accessible in other tables
     PRIMARY KEY (id)
 );
 
+-- create table for roles
 DROP TABLE IF EXISTS roles;
 CREATE TABLE roles (
     id INT AUTO_INCREMENT NOT NULL,
@@ -17,10 +22,12 @@ CREATE TABLE roles (
     salary DECIMAL NOT NULL,
     PRIMARY KEY(id),
     department_id INT NOT NULL,
+    -- accessing the id key in departments to be referenced in roles table
     FOREIGN KEY (department_id) 
     REFERENCES departments(department_id)
 );
 
+-- create table for employees
 DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
     id INT AUTO_INCREMENT NOT NULL,
